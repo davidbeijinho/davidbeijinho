a511ff8acc228ebeae9bdd500e679dc05ae2c19d
David Beijinho <davidbeijinho@gmail.com>
2020-03-24 17:55:13 +0100

    chore: first simple page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,64 +3,45 @@ import React from "react"
 // import Layout from "../components/layout"
 // import Image from "../components/image"
 // import SEO from "../components/seo"
-import Navigation from "../components/Navigation"
-import Heading from "../components/Heading"
-import Footer from "../components/Footer"
-import LeadCard from "../components/LeadCard"
-import Author from "../components/Author"
-import Subscribe from "../components/Subscribe"
-import Post from "../components/Post"
+// import Navigation from "../components/Navigation"
+// import Heading from "../components/Heading"
+// import Footer from "../components/Footer"
+// import LeadCard from "../components/LeadCard"
+// import Author from "../components/Author"
+// import Subscribe from "../components/Subscribe"
+// import Post from "../components/Post"
+import me from "../images/david_square.jpeg";
+
 
 const IndexPage = () => (
-  <div className="bg-gray-200 font-sans leading-normal tracking-normal">
-    {/* <Layout> */}
-    {/* <SEO title="Home" />
-<h1>Hi people</h1>
-<p>Welcome to your new Gatsby site.</p>
-<p>Now go build something great.</p>
-<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-<Image />
-</div>
-<Link to="/page-2/">Go to page 2</Link>
-<div className="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
-<div className="flex-shrink-0">
-<Image className="h-12 w-12"  alt="ChitChat Logo"/>
-</div>
-<div className="ml-6 pt-1">
-<h4 className="text-xl text-gray-900 leading-tight">ChitChat</h4>
-<p className="text-base text-gray-600 leading-normal">
-You have a new message!
-</p>
-</div>
-</div> */}
-
-    <Heading />
-
-    <div className="container px-4 md:px-0 max-w-6xl mx-auto -mt-32">
-      <div className="mx-0 sm:mx-6">
-        {/* <Navigation /> */}
-
-        {/* <div className="bg-gray-200 w-full text-xl md:text-2xl text-gray-800 leading-normal rounded-t"> */}
-          <LeadCard />
-
-          {/* <div className="flex flex-wrap justify-between pt-12 -mx-6">
-            <Post wClass="md:w-1/3" />
-            <Post wClass="md:w-1/3" />
-            <Post wClass="md:w-1/3" />
-            <Post wClass="md:w-1/2" />
-            <Post wClass="md:w-1/2" />
-            <Post wClass="md:w-2/3" />
-            <Post wClass="md:w-1/3" />
-          </div> */}
-        {/* </div> */}
-
-        {/* <Subscribe />
-        <Author /> */}
+  <div
+    style={{ height: "min-content" }}
+    className="w-full flex items-center justify-center my-auto"
+  >
+    <div className="rounded rounded-t-lg overflow-hidden shadow max-w-xs my-3">
+      <div className="flex justify-center m-8">
+        <img
+          src={me}
+          className="rounded-full border-solid border-white border-2 -mt-3"
+        />
       </div>
+      <div className="text-center px-3 pb-6 pt-2">
+        <h3 className="text-black text-sm bold font-sans">Hi there im David Beijinho</h3>
+        <p className="mt-2 font-sans font-light text-grey-dark">
+        Hello, I am a Web developer, and I like to develop for different media and interactivity experiences. Recently i have been working for a big company running a lot of AB testing, in an agile environment, i also have experience developing a lot of API's and create Web apps using micro-services, but I also enjoy digital fabrication, especially 3D printing, and I have some small projects for the IOT. using physical computing.
+        </p>
+      </div>
+      {/* <div className="flex justify-center pb-3 text-grey-dark">
+        <div className="text-center mr-3 border-r pr-3">
+          <h2>34</h2>
+          <span>Photos</span>
+        </div>
+        <div className="text-center">
+          <h2>42</h2>
+          <span>Friends</span>
+        </div>
+      </div> */}
     </div>
-
-    {/* <Footer /> */}
-    {/* </Layout> */}
   </div>
 )
 
